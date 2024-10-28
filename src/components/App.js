ccffe16d27b62670bde963f1ea95a7345fac99b1
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-10-28 14:18:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,21 @@
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const weather = {
+//     temperature: 25, 
+//     conditions: "Sunny"
+//   }
+//   return (
+//     <div>
+//         <p style={weather.temperature> 20?{backgroundColor:'red'}:{backgroundColor:'blue'}}>Temperature: {weather.temperature}</p>
+//         <span > Condition: {weather.conditions}</span>
+
+//     </div>
+//   )
+// }
+
+// export default App
 import React from "react";
 import './../styles/App.css';
 
@@ -8,9 +26,8 @@ const App = () => {
   }
   return (
     <div>
-        <p style={weather.temperature> 20?{backgroundColor:'red'}:{backgroundColor:'blue'}}>Temperature: {weather.temperature}</p>
-        <span > Condition: {weather.conditions}</span>
-
+        <p style={weather.temperature > 20 ? {color:'red'} : {color:'blue'}}>Temperature: {weather.temperature}</p>
+        <span> Condition: {weather.conditions}</span>
     </div>
   )
 }
